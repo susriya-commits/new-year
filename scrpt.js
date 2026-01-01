1545f3eef744186441be1da610d960dc3d10c39e
susriya-commits <susriya.kasibhotlaa@gmail.com>
2026-01-01 18:32:41 +0530

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,4 @@
+/* 🎉 Personal New Year Wish */
 function personalWish() {
   let name = document.getElementById("name").value;
 
@@ -7,3 +8,58 @@ function personalWish() {
     alert("🎉 Happy New Year " + name + "! 🎉");
   }
 }
+
+
+const canvas = document.getElementById("fireworks");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+let fireworks = [];
+
+function Firework(x, y) {
+  this.x = x;
+  this.y = y;
+  this.radius = Math.random() * 2 + 1;
+  this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  this.life = 60;
+}
+
+function createFirework() {
+  fireworks.push(
+    new Firework(
+      Math.random() * canvas.width,
+      Math.random() * canvas.height
+    )
+  );
+}
+
+function animateFireworks() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  fireworks.forEach((fw, index) => {
+    ctx.beginPath();
+    ctx.arc(fw.x, fw.y, fw.radius, 0, Math.PI * 2);
+    ctx.fillStyle = fw.color;
+    ctx.fill();
+
+    fw.life--;
+    fw.radius += 0.3;
+
+    if (fw.life <= 0) {
+      fireworks.splice(index, 1);
+    }
+  });
+
+  requestAnimationFrame(animateFireworks);
+}
+
+setInterval(createFirework, 300);
+animateFireworks();
+
